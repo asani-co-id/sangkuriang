ad0ce84a013dc45a42c3d7eba2ac75dc2ab21a4d
Muhammad Septian <muhamad.septian@asani.co.id>
2020-04-20 23:31:48 +0700

    init

---
+++ sangkuriang.go
@@ -0,0 +1,32 @@
+package sangkuriang
+
+import (
+	"bytes"
+	"database/sql"
+	"net/http"
+
+	"github.com/tidwall/gjson"
+	"github.com/uniplaces/carbon"
+)
+
+func GetBodyJson(r *http.Request) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	newStr := buf.String()
+	if !gjson.Valid(newStr) {
+		return ""
+	} else {
+		return newStr
+	}
+}
+
+func Suling(db *sql.DB, r *http.Request, id int) {
+	times, _ := carbon.NowInLocation("Asia/Jakarta")
+	logMainWhen := times.DateTimeString()
+	logMainRequest := GetBodyJson(r)
+	insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_request, log_rental_who, log_rental_when) VALUES (?, ?, ?, ?)")
+	if err != nil {
+		panic(err.Error())
+	}
+	go insForm.Exec(r.URL.Path, logMainRequest, id, logMainWhen)
+}
