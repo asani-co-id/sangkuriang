cdc845536616a516dba1b1a600aa79ede8682178
Muhammad Septian <muhamad.septian@asani.co.id>
2020-04-21 00:53:10 +0700

    balik

--- sangkuriang.go
+++ sangkuriang.go
@@ -1,26 +1,32 @@
 package sangkuriang
 
 import (
+	"bytes"
 	"database/sql"
-	"io/ioutil"
 	"net/http"
-	"sync"
 
+	"github.com/tidwall/gjson"
 	"github.com/uniplaces/carbon"
 )
 
-var mutex sync.RWMutex
+func GetBodyJson(r *http.Request) string {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+	newStr := buf.String()
+	if !gjson.Valid(newStr) {
+		return ""
+	} else {
+		return newStr
+	}
+}
 
 func Suling(db *sql.DB, r *http.Request, id int) {
-	mutex.Lock()
-	req, _ := r.GetBody()
 	times, _ := carbon.NowInLocation("Asia/Jakarta")
 	logMainWhen := times.DateTimeString()
-	logMainRequest, _ := ioutil.ReadAll(req)
+	logMainRequest := GetBodyJson(r)
 	insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_request, log_rental_who, log_rental_when) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
 	}
-	insForm.Exec(r.URL.Path, string(logMainRequest), id, logMainWhen)
-	mutex.Unlock()
+	go insForm.Exec(r.URL.Path, logMainRequest, id, logMainWhen)
 }
