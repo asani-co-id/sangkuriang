081b7a3b14081748bbfcad5113b051214653c10d
Muhammad Septian <muhamad.septian@asani.co.id>
2020-04-21 00:44:53 +0700

    req

--- sangkuriang.go
+++ sangkuriang.go
@@ -13,9 +13,10 @@ var mutex sync.RWMutex
 
 func Suling(db *sql.DB, r *http.Request, id int) {
 	mutex.Lock()
+	req, _ := r.GetBody()
 	times, _ := carbon.NowInLocation("Asia/Jakarta")
 	logMainWhen := times.DateTimeString()
-	logMainRequest, _ := ioutil.ReadAll(r.Body)
+	logMainRequest, _ := ioutil.ReadAll(req)
 	insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_request, log_rental_who, log_rental_when) VALUES (?, ?, ?, ?)")
 	if err != nil {
 		panic(err.Error())
