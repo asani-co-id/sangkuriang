919af888c62a30550d43161445a6bc1af9b715d8
Muhammad Septian <muhamad.septian@asani.co.id>
2020-04-21 10:10:49 +0700

    test

--- sangkuriang.go
+++ sangkuriang.go
@@ -4,16 +4,21 @@ import (
 	"bytes"
 	"database/sql"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 
 	"github.com/tidwall/gjson"
 )
 
-func GetBodyJson(r *http.Request) string {
+func GetBody(r *http.Request) string {
+	b := bytes.NewBuffer(make([]byte, 0))
+	reader := io.TeeReader(r.Body, b)
 	buf := new(bytes.Buffer)
-	buf.ReadFrom(r.Body)
+	buf.ReadFrom(reader)
 	newStr := buf.String()
+	defer r.Body.Close()
+	r.Body = ioutil.NopCloser(b)
 	if !gjson.Valid(newStr) {
 		return ""
 	} else {
@@ -22,8 +27,7 @@ func GetBodyJson(r *http.Request) string {
 }
 
 func Suling(db *sql.DB, r *http.Request, id int) {
-	b, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(b))
+	fmt.Println(GetBody(r))
 	// times, _ := carbon.NowInLocation("Asia/Jakarta")
 	// logMainWhen := times.DateTimeString()
 	// insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_who, log_rental_when) VALUES (?, ?, ?)")
