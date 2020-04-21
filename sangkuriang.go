6bb572c2ae8fb4e8e5b6bb2fe0d4b21899ec6000
Muhammad Septian <muhamad.septian@asani.co.id>
2020-04-21 09:58:36 +0700

    Test

--- sangkuriang.go
+++ sangkuriang.go
@@ -3,10 +3,12 @@ package sangkuriang
 import (
 	"bytes"
 	"database/sql"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/tidwall/gjson"
-	"github.com/uniplaces/carbon"
 )
 
 func GetBodyJson(r *http.Request) string {
@@ -21,11 +23,16 @@ func GetBodyJson(r *http.Request) string {
 }
 
 func Suling(db *sql.DB, r *http.Request, id int) {
-	times, _ := carbon.NowInLocation("Asia/Jakarta")
-	logMainWhen := times.DateTimeString()
-	insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_who, log_rental_when) VALUES (?, ?, ?)")
-	if err != nil {
-		panic(err.Error())
-	}
-	go insForm.Exec(r.URL.Path, id, logMainWhen)
+	b := bytes.NewBuffer(make([]byte, 0))
+	io.TeeReader(r.Body, b)
+	defer r.Body.Close()
+	r.Body = ioutil.NopCloser(b)
+	fmt.Println(b.String)
+	// times, _ := carbon.NowInLocation("Asia/Jakarta")
+	// logMainWhen := times.DateTimeString()
+	// insForm, err := db.Prepare("INSERT INTO log_rental (log_rental_url, log_rental_who, log_rental_when) VALUES (?, ?, ?)")
+	// if err != nil {
+	// 	panic(err.Error())
+	// }
+	// go insForm.Exec(r.URL.Path, id, logMainWhen)
 }
